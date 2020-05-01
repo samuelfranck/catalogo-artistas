315f1fde3e8050f84b4bf98c8cbbc27ebf0581ba
Samuel Franck <samuelfranck.adm@gmail.com>
2020-05-01 10:54:00 -0300

    Criado as classes de domínio Artista, Música e Álbum

---
+++ src/main/java/com/samuelfranck/catalagoartistas/domain/Album.java
@@ -0,0 +1,73 @@
+package com.samuelfranck.catalagoartistas.domain;
+
+import java.io.Serializable;
+
+public class Album implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private Integer ano;
+	
+	public Album() {
+	}
+
+	public Album(Integer id, String nome, Integer ano) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.ano = ano;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Album other = (Album) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}

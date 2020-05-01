0b579b5aa5f758a52c3f996bea26f9a4de2b5a67
Samuel Franck <samuelfranck.adm@gmail.com>
2020-05-01 10:34:00 -0300

    Projeto Catálogo de Artistas criado

---
+++ src/main/java/com/samuelfranck/catalagoartistas/CatalagoArtistasApplication.java
@@ -0,0 +1,13 @@
+package com.samuelfranck.catalagoartistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalagoArtistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalagoArtistasApplication.class, args);
+	}
+
+}
